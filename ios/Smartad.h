dfd483c919d9de55224c99dffc7ab0da2d157476
Henri Roumegoux <henri@hicsuntdev.com>
2020-03-10 15:13:03 +0100

    [ios] objective c bridge

--- ios/Smartad.h
+++ ios/Smartad.h
@@ -1,9 +1,31 @@
-#import <React/RCTBridgeModule.h>
 #import <UIKit/UIKit.h>
+#import <SASDisplayKit/SASDisplayKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+#if __has_include(<React/RCTEventEmitter.h>)
+#import <React/RCTEventEmitter.h>
+#else
+#import "RCTEventEmitter.h"
+#endif
 
-@property (strong, nonatomic) UIWindow *window;
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 
+NS_ASSUME_NONNULL_BEGIN
+
+/*
+ The purpose of this view controller is to display a rewarded video.
+ 
+ A rewarded video works like an interstitial but is optimized to display video
+ that will yield a reward when watched until the end.
+ */
+@interface Smartad : RCTEventEmitter <RCTBridgeModule, ISRewardedVideoDelegate>
+// @interface Smartad : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *showRewardedVideoAdButton;
 
 @end
+
+NS_ASSUME_NONNULL_END
--- ios/Smartad.m
+++ ios/Smartad.m
@@ -1,17 +1,125 @@
 #import "Smartad.h"
-#import <SASDisplayKit/SASDisplayKit.h>
 
-#define kSiteID 104808
+NSString *const kSmartAdRewardedVideoNotReady = @"smartAdRewardedVideoNotReady";
+NSString *const kSmartAdRewardedVideoAdLoaded = @"smartAdRewardedVideoAdLoaded";
+NSString *const kSmartAdRewardedVideoAdFailedToLoad = @"smartAdRewardedVideoAdFailedToLoad";
+NSString *const kSmartAdRewardedVideoAdShown = @"smartAdRewardedVideoAdShown";
+NSString *const kSmartAdVideoAdFailedToShow = @"smartAdVideoAdFailedToShow";
+NSString *const kSmartAdRewardedVideoAdClosed = @"smartAdRewardedVideoAdClosed";
+NSString *const kSmartAdRewardReceived = @"smartAdRewardReceived";
+NSString *const kSmartAdRewardNotReceived = @"smartAdRewardNotReceived";
+NSString *const kSmartAdRewardedVideoAdClicked = @"smartAdRewardedVideoAdClicked";
+NSString *const kSmartAdRewardedVideoEvent = @"smartAdRewardedVideoEvent";
+NSString *const kSmartAdRewardedVideoEndCardDisplayed = @"smartAdRewardedVideoEndCardDisplayed";
+
 #define kBaseURL @"https://mobile.smartadserver.com"
 
-@implementation Smartad
+@interface Smartad () <SASRewardedVideoManagerDelegate>
+
+@property SASRewardedVideoManager *rewardedVideoManager;
+
+@end
+
+@implementation Smartad {
+    RCTRespconseSenderBlock _requestRewardedVideoCallback;
+}
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions)
-{
-  [[SASConfiguration sharedInstance] configureWithSiteId:kSiteID baseURL:kBaseURL];);
+- (NSArray<NSString *> *)supportedEvents {
+    return @[
+        kSmartAdRewardedVideoNotReady
+        kSmartAdRewardedVideoAdLoaded
+        kSmartAdRewardedVideoAdFailedToLoad
+        kSmartAdRewardedVideoAdShown
+        kSmartAdVideoAdFailedToShow
+        kSmartAdRewardedVideoAdClosed
+        kSmartAdRewardReceived
+        kSmartAdRewardNotReceived
+        kSmartAdRewardedVideoAdClicked
+        kSmartAdRewardedVideoEvent
+        kSmartAdRewardedVideoEndCardDisplayed
+    ];
 }
 
+RCT_EXPORT_METHOD(initializeRewardedVideo:(nonnull NSInteger *)kRewardedVideoSiteID kRewardedVideoPageID:(nonnull NSString *)kRewardedVideoPageID kRewardedVideoFormatID:(nonnull NSInteger *)kRewardedVideoFormatID kRewardedVideoKeywordTargeting:(nullable NSString *)kRewardedVideoKeywordTargeting)
+{
+    [[SASConfiguration sharedInstance] configureWithSiteId:kRewardedVideoSiteID baseURL:kBaseURL];);
+    SASAdPlacement *placement = [SASAdPlacement
+        adPlacementWithSiteId:kRewardedVideoSiteID
+                       pageId:kRewardedVideoPageID
+                     formatId:kRewardedVideoFormatID
+             keywordTargeting:kRewardedVideoKeywordTargeting
+    ];
+    self.rewardedVideoManager = [[SASRewardedVideoManager alloc] initWithPlacement:placement delegate:self];
+}
+
+RCT_EXPORT_METHOD(loadRewardedVideoAd)
+{
+    [self.rewardedVideoManager load];
+}
+
+RCT_EXPORT_METHOD(showRewardedVideo)
+{
+    if (self.rewardedVideoManager.adStatus == SASAdStatusReady) {
+        [self.rewardedVideoManager showFromViewController:self];
+    } else if (self.rewardedVideoManager.adStatus == SASAdStatusExpired) {
+        NSLog(@"RewardedVideo has expired and cannot be shown anymore.");
+        [self sendEventWithName:kSmartAdRewardedVideoNotReady body:nil];
+    } else {
+        [self sendEventWithName:kSmartAdRewardedVideoNotReady body:nil];
+    }
+}
+
+
+- (void)rewardedVideoManager:(SASRewardedVideoManager *)manager didLoadAd: (SASAd *)ad {
+    NSLog(@"RewardedVideo has been loaded and is ready to be shown");
+    [self sendEventWithName:kSmartAdRewardedVideoAdLoaded body:nil];
+}
+
+- (void)rewardedVideoManager:(SASRewardedVideoManager *)manager didFailToLoadWithError: (NSError *)error {
+    NSLog(@"RewardedVideo did fail to load with error: %@", [error localizedDescription]);
+    [self sendEventWithName:kSmartAdRewardedVideoAdFailedToLoad body:nil];
+}
+
+- (void)rewardedVideoManager:(SASRewardedVideoManager *)manager didAppearFromViewController: (UIViewController *)controller {
+    [self sendEventWithName:kSmartAdRewardedVideoAdShown body:nil];
+}
+
+- (void)rewardedVideoManager:(SASRewardedVideoManager *)manager didFailToShowWithError: (NSError *)error {
+    NSLog(@"RewardedVideo did fail to show with error: %@", [error localizedDescription]);
+    [self sendEventWithName:kSmartAdVideoAdFailedToShow body:nil];
+}
+
+- (void)rewardedVideoManager:(SASRewardedVideoManager *)manager didDisappearFromViewController: (UIViewController *)controller {
+    [self sendEventWithName:kSmartAdRewardedVideoAdClosed body:nil];
+}
+
+- (void)rewardedVideoManager:(SASRewardedVideoManager *)manager didCollectReward: (SASReward *)reward {
+    if (reward != nil) {
+        NSLog(@"RewardedVideo did collect reward for currency %@ with amount %ld", reward.currency, (long)[reward.amount integerValue]);
+        [self sendEventWithName:kSmartAdRewardReceived body:nil];
+    }
+    [self sendEventWithName:kSmartAdRewardNotReceived body:nil];
+}
+
+- (void)rewardedVideoManager:(SASRewardedVideoManager *)manager shouldHandleURL: (NSURL *)URL {
+    [self sendEventWithName:kSmartAdRewardedVideoAdClicked body:nil];
+
+}
+
+- (void)rewardedVideoManager:(SASRewardedVideoManager *)manager didSendVideoEvent: (SASVideoEvent *)videoEvent {
+    NSLog(@"RewardedVideo did send video event: %li", (long)videoEvent);
+    [self sendEventWithName:kSmartAdRewardedVideoEvent body:nil];
+}
+
+- (void)rewardedVideoManager:(SASRewardedVideoManager *)manager didLoadEndCardFromViewController: (UIViewController *)controller {
+    [self sendEventWithName:kSmartAdRewardedVideoEndCardDisplayed body:nil];
+}
 
 @end
