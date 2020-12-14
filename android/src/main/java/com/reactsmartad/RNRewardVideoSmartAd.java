a56dccf4175b4c6590e316f302847445bb9f9335
AlexandreLac <alexandrelac248@gmail.com>
2020-12-14 11:05:28 +0100

    Replace smart module by RNRewardVideo

--- android/src/main/java/com/reactsmartad/RNRewardVideoSmartAd.java
+++ android/src/main/java/com/reactsmartad/RNRewardVideoSmartAd.java
@@ -52,7 +52,7 @@ public class RNRewardVideoSmartAd extends ReactContextBaseJavaModule {
      ****************************/
     
 
-    public SmartadModule(ReactApplicationContext reactContext) {
+    public RNRewardVideoSmartAd(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
     }
@@ -102,7 +102,7 @@ public class RNRewardVideoSmartAd extends ReactContextBaseJavaModule {
         if (mRewardedVideoManager != null && mRewardedVideoManager.getAdStatus() == SASAdStatus.READY) {
             mRewardedVideoManager.showRewardedVideo();
         } else {
-            Log.e(SmartadModule.TAG, "RewardedVideo is not ready for the current placement.");
+            Log.e(RNRewardVideoSmartAd.TAG, "RewardedVideo is not ready for the current placement.");
             sendEvent("smartAdRewardedVideoNotReady", null);
         }
     }
@@ -110,7 +110,7 @@ public class RNRewardVideoSmartAd extends ReactContextBaseJavaModule {
         this.mRewardedVideoListener = new SASRewardedVideoManager.RewardedVideoListener() {
             @Override
             public void onRewardedVideoAdLoaded(SASRewardedVideoManager rewardedVideoManager, SASAdElement adElement) {
-                Log.i(SmartadModule.TAG, "RewardedVideo Ad loading completed.");
+                Log.i(RNRewardVideoSmartAd.TAG, "RewardedVideo Ad loading completed.");
 
                 SASNativeVideoAdElement castedAd = (SASNativeVideoAdElement)adElement;
                 if (!castedAd.getPosterImageUrl().isEmpty()) {
@@ -132,7 +132,7 @@ public class RNRewardVideoSmartAd extends ReactContextBaseJavaModule {
 
             @Override
             public void onRewardedVideoAdFailedToLoad(SASRewardedVideoManager rewardedVideoManager, Exception exception) {
-                Log.i(SmartadModule.TAG, "RewardedVideo Ad loading failed with exception: " + exception.getLocalizedMessage());
+                Log.i(RNRewardVideoSmartAd.TAG, "RewardedVideo Ad loading failed with exception: " + exception.getLocalizedMessage());
                 WritableMap params = Arguments.createMap();
                 params.putString("message", exception.getLocalizedMessage());
                 sendEvent("smartAdRewardedVideoAdFailedToLoad", params);
@@ -140,27 +140,27 @@ public class RNRewardVideoSmartAd extends ReactContextBaseJavaModule {
 
             @Override
             public void onRewardedVideoAdShown(SASRewardedVideoManager rewardedVideoManager) {
-                Log.i(SmartadModule.TAG, "RewardedVideo ad is shown.");
+                Log.i(RNRewardVideoSmartAd.TAG, "RewardedVideo ad is shown.");
                 sendEvent("smartAdRewardedVideoAdShown", null);
             }
 
             @Override
             public void onRewardedVideoAdFailedToShow(SASRewardedVideoManager rewardedVideoManager, Exception exception) {
-                Log.i(SmartadModule.TAG, "RewardedVideo playback failed with exception: " + exception.getLocalizedMessage());
+                Log.i(RNRewardVideoSmartAd.TAG, "RewardedVideo playback failed with exception: " + exception.getLocalizedMessage());
                 sendEvent("smartAdVideoAdFailedToShow", null);
             }
 
             @Override
             public void onRewardedVideoAdClosed(SASRewardedVideoManager rewardedVideoManager) {
-                Log.i(SmartadModule.TAG, "RewardedVideo closed.");
+                Log.i(RNRewardVideoSmartAd.TAG, "RewardedVideo closed.");
                 sendEvent("smartAdRewardedVideoAdClosed", null);
             }
 
             @Override
             public void onRewardReceived(SASRewardedVideoManager rewardedVideoManager, SASReward reward) {
                 if (reward != null) {
-                    Log.i(SmartadModule.TAG, "RewardedVideo collected a reward.");
-                    Log.i(SmartadModule.TAG, "User should be rewarded with: " + reward.getAmount() + " " + reward.getCurrency() + ".");
+                    Log.i(RNRewardVideoSmartAd.TAG, "RewardedVideo collected a reward.");
+                    Log.i(RNRewardVideoSmartAd.TAG, "User should be rewarded with: " + reward.getAmount() + " " + reward.getCurrency() + ".");
                     WritableMap params = Arguments.createMap();
                     params.putDouble("amount", reward.getAmount());
                     params.putString("currency", reward.getCurrency());
@@ -173,19 +173,19 @@ public class RNRewardVideoSmartAd extends ReactContextBaseJavaModule {
 
             @Override
             public void onRewardedVideoAdClicked(SASRewardedVideoManager rewardedVideoManager) {
-                Log.i(SmartadModule.TAG, "RewardedVideo clicked.");
+                Log.i(RNRewardVideoSmartAd.TAG, "RewardedVideo clicked.");
                 sendEvent("smartAdRewardedVideoAdClicked", null);
             }
 
             @Override
             public void onRewardedVideoEvent(SASRewardedVideoManager rewardedVideoManager, int videoEvent) {
-                Log.i(SmartadModule.TAG, "RewardedVideo did send event: " + videoEvent);
+                Log.i(RNRewardVideoSmartAd.TAG, "RewardedVideo did send event: " + videoEvent);
                 sendEvent("smartAdRewardedVideoEvent", null);
             }
 
             @Override
             public void onRewardedVideoEndCardDisplayed(SASRewardedVideoManager rewardedVideoManager, ViewGroup endCardView) {
-                Log.i(SmartadModule.TAG, "RewardedVideo HTML EndCard displayed.");
+                Log.i(RNRewardVideoSmartAd.TAG, "RewardedVideo HTML EndCard displayed.");
                 sendEvent("smartAdRewardedVideoEndCardDisplayed", null);
             }
         };
